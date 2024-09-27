15c1d1a2821b899e8c75b4b8e24dab19b97e3d8c
voidKandy <voidkandy@gmail.com>
2024-09-27 13:38:48 -0700

    changed DATABASE_URL env var name to DB_URL

--- src/state.rs
+++ src/state.rs
@@ -24,15 +24,11 @@ pub struct Config {
 impl Config {
     pub fn init() -> Config {
         dotenv().ok();
-        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let database_url = std::env::var("DB_URL").expect("DATABASE_URL must be set");
         let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
-        // let jwt_expires_in = std::env::var("JWT_EXPIRED_IN").expect("JWT_EXPIRED_IN must be set");
-        // let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
         Config {
             database_url,
             jwt_secret,
-            // jwt_expires_in,
-            // jwt_maxage: jwt_maxage.parse::<i32>().unwrap(),
         }
     }
 }
