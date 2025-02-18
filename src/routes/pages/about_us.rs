7b79ad0f0a6ba81f5f42e2afc6a0c76124b8f509
voidKandy <voidkandy@gmail.com>
2025-02-18 13:27:02 -0800

    updated board member

--- src/routes/pages/about_us.rs
+++ src/routes/pages/about_us.rs
@@ -30,13 +30,36 @@ fn board_members() -> Vec<BoardMember> {
         BoardMember {
             name: "Beverly L. Martin-Ornelas".to_string(),
             role: "Board Member".to_string(),
-            description: "This is a board member".to_string(),
+            description: r#"
+            Beverly L. Martin comes to Semper Flies Foundation with a huge heart for Veterans. A
+            United States Marine raised her, WWII &amp; Korean War Combat Veteran, my grandfather
+            Verne Norman Martin, Jr. Beverly volunteers for Semper Flies Foundation as CFO with
+            her financial expertise. Beverly owned &amp; operated one of the largest Trust Deed
+            Investment Companies in Modesto, CA. for 32 years. She was “Businesswoman of the
+            Year” in Modesto Ca promoted by The Modesto Bee. She raised three sons, all of whom
+            went on to serve our country in the armed forces. Watching her father come back from
+            WWII &amp; The Korean War and battle PTSD every day created her capacity to understand
+            what Combat Veterans returning from theatre have to go through. She witnessed her
+            father’s struggle with these conditions. Beverly was a massive part of Jamie Guajardo&#39;s
+            treatment and remains a considerable part of his support network today. Along with Dan
+            Ornelas Sr., Beverly’s husband, and our other board members, we feel that raising
+            awareness for Veterans who struggle with PTSD &amp; TBI is a top priority.
+            "#.to_string(),
             image_url: "public/assets/images/board_members/beverly.webp".to_string(),
         },
         BoardMember {
             name: "Dan Ornelas".to_string(),
             role: "Board Member".to_string(),
-            description: "This is a board member".to_string(),
+            description: r#"
+            Dan Ornelas Sr. comes to Semper Flies Foundation with an understanding of the war
+            plaguing our Veterans today. With Beverly, he raised three sons who served in the
+            armed forces. Dan is the epitome of a Christian Man. He instilled in his sons the
+            importance of walking with Jesus Christ. Dan is the CEO of a trust deed investment
+            company today, MFS Preferred, that serves a private portfolio in the Modesto area. He
+            also played a massive part in getting Jamie Guajardo the help he desperately needed
+            after returning from Iraq. He is a significant part of Jamie’s support network today. Like
+            Beverly, he is committed to raising awareness for Veterans who battle PTSD &amp; TBI.                
+            "#.to_string(),
             image_url: "public/assets/images/board_members/dan.webp".to_string(),
         },
     ]
